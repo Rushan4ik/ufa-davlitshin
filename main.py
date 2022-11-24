f665348bf37cc9ad50e18edea6b86bc71e714a4c
rushan <rushan.davlitshin.0809@gmail.com>
2022-11-24 18:31:56 +0500

    Coffee first and requirements.txt

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import random
+import sqlite3
 import sys
 
 from PyQt5 import QtGui
 from PyQt5.QtGui import QPainter, QColor
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
 from PyQt5.QtCore import QRect
 from PyQt5.uic import loadUi
 
@@ -12,28 +13,21 @@ class MyWidget(QWidget):
     def __init__(self):
         super().__init__()
         self.c = []
-        loadUi("UI.ui", self)
+        loadUi("main.ui", self)
+        self.init_data()
         self.init_ui()
 
+    def init_data(self):
+        connection = sqlite3.connect("coffee.sqlite")
+        self.result = [data for data in connection.execute("select * from coffees").fetchall()]
+        connection.close()
+
     def init_ui(self):
-        self.setFixedSize(800, 800)
-        self.pushButton.clicked.connect(self.handle)
-
-    def handle(self):
-        x, y = random.randint(200, 800), random.randint(200, 800)
-        rad = random.randint(1, 200)
-        r, g, b = [random.randint(0, 255) for i in range(3)]
-        self.c.append(((r, g, b), QRect(x - rad, y - rad, rad, rad)))
-        self.repaint()
-
-    def paintEvent(self, a0: QtGui.QPaintEvent) -> None:
-        painter = QPainter()
-        painter.begin(self)
-        for color, rect in self.c:
-            r, g, b = color
-            painter.setBrush(QColor(r, g, b))
-            painter.drawEllipse(rect)
-        painter.end()
+        for d in self.result:
+            d = ', '.join(map(str, d))
+            label = QLabel(d, self)
+            label.setFixedSize(800, 20)
+            self.verticalLayout.addWidget(label)
 
 
 def main():
